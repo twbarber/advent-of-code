72507c6a6e61fe4cf7d1e05def19519667c5fef2
Tyler Barber <dev@twbarber.com>
2017-12-03 21:22:18 -0500

    Pushing to finish tonight.

--- src/main/kotlin/me/twbarber/advent/Day3.kt
+++ src/main/kotlin/me/twbarber/advent/Day3.kt
@@ -1,2 +1,13 @@
 package me.twbarber.advent
 
+fun findCoordinates(target: Int) : Pair<Int, Int> {
+        val square = findSquare(0, target)
+
+}
+
+fun findSquare(cur: Int, target: Int) : Int =
+        if (findMax(cur) < target) findSquare(cur + 1, target) else cur
+
+
+fun findMax(cur: Int) : Int = if(cur == 0)  1 else (8 * (cur)) + findMax(cur - 1)
+
--- src/test/kotlin/me/twbarber/advent/Day3Test.kt
+++ src/test/kotlin/me/twbarber/advent/Day3Test.kt
@@ -1,36 +1,48 @@
 package me.twbarber.advent
 
-import org.junit.Assert.assertEquals
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class Day3Test {
 
-    private val INPUT_FILE = "input/day_two.txt"
-    private val input = ResourceLoader.loadAsText(INPUT_FILE).trimMargin()
-
     @Test
-    fun partOne() {
-        val testData = """
-            |5 1 9 5
-            |7 5 3
-            |2 4 6 8
-        """.trimMargin()
-        assertEquals(18, dayTwoPartOne(testData.format()))
-        assertEquals(34581, dayTwoPartOne(input.format()))
+    fun `findMax square 0`() {
+        assertEquals(1, findMax(0))
     }
 
     @Test
-    fun partTwo() {
-        val testData = """
-            |5 9 2 8
-            |9 4 7 3
-            |3 8 6 5
-        """.trimMargin()
-        assertEquals(9, dayTwoPartTwo(testData.format()))
-        assertEquals(214, dayTwoPartTwo(input.format()))
+    fun `findMax square 1`() {
+        assertEquals(9, findMax(1))
     }
 
-    private fun String.format() = this.split("\n").map { it.split(" ").map { it.toInt() } }
+    @Test
+    fun `findMax square 2`() {
+        assertEquals(25, findMax(2))
+    }
 
+    @Test
+    fun `findSquare max 1`() {
+        assertEquals(0, findSquare(0, 1))
+    }
+
+    @Test
+    fun `findSquare max 9`() {
+        assertEquals(1, findSquare(0, 9))
+    }
+
+    @Test
+    fun `findSquare max 49`() {
+        assertEquals(3, findSquare(0, 49))
+    }
+
+    @Test
+    fun `findSquare max 72`() {
+        assertEquals(4, findSquare(0, 72))
+    }
+
+    @Test
+    fun `findSquare max 81`() {
+        assertEquals(4, findSquare(0, 81))
+    }
 
 }
