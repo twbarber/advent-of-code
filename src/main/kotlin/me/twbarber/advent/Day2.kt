f2f393a2fc3dbe84e24f638d460cb24155ddb995
Tyler Barber <tyler@rocketmiles.com>
2017-12-07 10:42:26 -0600

    Golf.

--- src/main/kotlin/me/twbarber/advent/Day2.kt
+++ src/main/kotlin/me/twbarber/advent/Day2.kt
@@ -6,7 +6,7 @@ typealias Row = List<Int>
 fun dayTwoPartOne(input: Spreadsheet) = input.checksum(Row::minMaxDif)
 fun dayTwoPartTwo(input: Spreadsheet) = input.checksum(Row::pairEvenDiv)
 
-fun Spreadsheet.checksum(func: (Row) -> Int) = map(func).sum()
+fun Spreadsheet.checksum(func: (Row) -> Int) = sumBy(func)
 
 fun Row.minMaxDif() = (max() ?: 0) - (min() ?: 0)
-fun Row.pairEvenDiv() = fold(0) { i, v -> i + filter { v % it == 0 && it != v }.sumBy { v / it } }
+fun Row.pairEvenDiv() = sumBy { v -> filter { v % it == 0 && it != v }.sumBy { v / it } }
